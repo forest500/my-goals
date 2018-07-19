7cdf75725c0f75c9ebf626a8ddbe97dfbef354e8
Miro <lasekmiroslaw@gmail.com>
2018-07-19 19:19:48 +0200

    remove hardcoded host and add from location

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    API_LOCATION: 'http://localhost:8000/api/',
-    AUTH_LOCATION: 'http://localhost:8000/api/login_check',
-    REGISTER_LOCATION: 'http://localhost:8000/api/register',
+    API_LOCATION: window.location.origin + '/api/',
+    AUTH_LOCATION: window.location.origin + '/api/login_check',
+    REGISTER_LOCATION: window.location.origin + '/api/register',
 }
